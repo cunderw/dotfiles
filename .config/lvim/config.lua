87968810c7790567461a32227fad7c9ffdd91e5a
Carson Underwood <cunderw@gmail.com>
2022-08-02 14:22:01 -0500

    update coolor scheme

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -11,7 +11,7 @@ an executable
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save = true
-lvim.colorscheme = "dracula"
+lvim.colorscheme = "tokyonight"
 lvim.transparent_window = true
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
@@ -148,6 +148,7 @@ lvim.builtin.treesitter.highlight.enabled = true
 -- Additional Plugins
 lvim.plugins = {
   { "Mofiqul/dracula.nvim" },
+  { "folke/tokyonight.nvim" },
   { "christoomey/vim-tmux-navigator" },
 }
 
